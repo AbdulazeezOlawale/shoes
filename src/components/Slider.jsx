9ee9eaf4ed1e37ecc538ade096564df05c387e00
AbdulazeezOlawale <orionabq@gmail.com>
2024-10-28 09:50:46 +0100

    updated range-slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from "../styles/slider.module.css"
 
 const Slider = () => {
+    const [sliderValue, setSliderValue] = useState(0);
+
+    const handleSliderChange = (event) => {
+        setSliderValue(parseInt(event.target.value, 0))
+    }
+
     return (
         <div>
-            
+            <input 
+            className={styles.range_input}
+            type="range"
+            id='rangeInput'
+            min="0"
+            max="1000"
+            value={sliderValue}
+            onChange={handleSliderChange}
+            />
+
+            <div className={styles.range_text}>
+                <span >
+                    $ {sliderValue}
+                </span>
+                <span>
+                    $ 1000
+                </span>
+            </div>
         </div>
     )
 }
